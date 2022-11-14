d67d61926927ed476c2a59f7fbb1808fdd573e49
dprater1 <daivionprates@gmail.com>
2022-11-14 12:06:56 -0800

    fixed endpoints and fixed some useless code

--- src/main/java/com/cognixia/jump/model/User.java
+++ src/main/java/com/cognixia/jump/model/User.java
@@ -10,11 +10,8 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
-import com.cognixia.jump.model.User.Role;
 
 @Entity
 public class User implements Serializable {
