58fc25a1db81e84126466154afab4b4dcc1b4944
I-The-Bill <blowery96@icloud.com>
2022-11-14 12:53:14 -0500

    security updates

--- src/main/java/com/cognixia/jump/service/MyUserDetailsService.java
+++ src/main/java/com/cognixia/jump/service/MyUserDetailsService.java
@@ -1,11 +1,10 @@
 package com.cognixia.jump.service;
 
-import java.util.Collection;
-import java.util.List;
+
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
+
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
