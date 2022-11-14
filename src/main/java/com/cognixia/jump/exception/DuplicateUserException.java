1276a34e25fc9a22a28f128d3a57d3b6633bbe42
Dongwoon Jeong <dongwoon.dj@gmail.com>
2022-11-14 11:28:09 -0600

    duplicateuserexception added

---
+++ src/main/java/com/cognixia/jump/exception/DuplicateUserException.java
@@ -0,0 +1,12 @@
+package com.cognixia.jump.exception;
+
+public class DuplicateUserException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+	
+	public DuplicateUserException(String msg) {
+		super(msg);
+	}
+	
+
+}
