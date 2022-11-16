d7defd4017ed2e344dc4910e03b42829e8ae5595
Dongwoon Jeong <dongwoon.dj@gmail.com>
2022-11-16 13:23:05 -0600

    get all cypher added

--- src/test/java/com/cognixia/jump/controller/CypherControllerTest.java
+++ src/test/java/com/cognixia/jump/controller/CypherControllerTest.java
@@ -1,6 +1,13 @@
 package com.cognixia.jump.controller;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.*;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -8,10 +15,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.cognixia.jump.filter.JwtRequestFilter;
+import com.cognixia.jump.model.Cyphers;
 import com.cognixia.jump.repository.CyphersRepository;
 import com.cognixia.jump.service.CyphersService;
 import com.cognixia.jump.service.MyUserDetailsService;
@@ -34,8 +43,52 @@ class CypherControllerTest {
 	
 	
 	@Test
-	void test() {
+	@WithMockUser
+	void testGetAllCyphers() throws Exception {
+		String uri = "/cyphers/all";
+		List<Cyphers> cypher = new ArrayList<Cyphers>();
+		
+		cypher.add(new Cyphers(1L, "answer", "ceaser","hints","easy",null, null));
+		when(service.getAllCyphers()).thenReturn(cypher);
+		
+		mockMvc.perform( get(uri) )
+		.andDo( print() )
+		.andExpect( status().isOk() )
+		.andExpect( jsonPath("$.length()").value( cypher.size() ) )
+		.andExpect( jsonPath("$[0].id").value( cypher.get(0).getId()) )
+		.andExpect( jsonPath("$[0].answer").value( cypher.get(0).getAnswer() ) )
+		;
+	}
+	
+	@Test
+	void testCreateProduct() {
 		fail("Not yet implemented");
 	}
+
+	@Test
+	void testGetCypherById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUpdateProduct() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetCypherEasy() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetCypherMedium() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetCypherHard() {
+		fail("Not yet implemented");
+	}
+
 
 }
